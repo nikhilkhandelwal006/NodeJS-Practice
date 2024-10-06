6c94e13c2ebc81338a1d0eddf7438277beffa27d
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2024-10-06 12:16:47 +0530

    EJS - server side rendering

---
+++ Chapter-10/controller/product.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const model = require('../model/product')
+const mongoose = require('mongoose');
+const Product = model.Product;
+const ejs = require('ejs');
+const path = require('path');
+
+//view
+exports.getAllProductsSSR = async (req, res) => {
+  const products = await Product.find();
+  ejs.renderFile(path.resolve(__dirname,'../pages/index.ejs'), {products:products}, function(err, str){
+    res.send(str);
+ });
+  
+};
+
+exports.getAddForm = async (req, res) => {
+  ejs.renderFile(path.resolve(__dirname,'../pages/add.ejs'), function(err, str){
+    res.send(str);
+ });
+  
+};
+
+
+
+// Create
+
+exports.createProduct = async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    const savedProduct = await product.save();  // Await the save method
+    res.status(201).json(savedProduct);         // Send the saved product as response
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);                  // Send the error if any occurs
+  }
+};
+
+
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+};
+
+exports.getProduct = async (req, res) => {
+  const id = req.params.id;
+  console.log({id})
+  const product = await Product.findById(id);
+  res.json(product);
+};
+exports.replaceProduct = async (req, res) => {
+  const id = req.params.id;
+  try{
+  const doc = await Product.findOneAndReplace({_id:id},req.body,{new:true})
+  res.status(201).json(doc);
+  }
+  catch(err){
+    console.log(err);
+    res.status(400).json(err);
+  }
+};
+exports.updateProduct = async (req, res) => {
+  const id = req.params.id;
+  try{
+  const doc = await Product.findOneAndUpdate({_id:id},req.body,{new:true})
+  res.status(201).json(doc);
+  }
+  catch(err){
+    console.log(err);
+    res.status(400).json(err);
+  }
+};
+exports.deleteProduct = async (req, res) => {
+  const id = req.params.id;
+  try{
+  const doc = await Product.findOneAndDelete({_id:id})
+  res.status(201).json(doc);
+  }
+  catch(err){
+    console.log(err);
+    res.status(400).json(err);
+  }
+};
